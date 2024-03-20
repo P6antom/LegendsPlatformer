6f902f6659d424ad0a750e294318e965c6c575c8
P6antom <pascalkaiser55@gmail.com>
2024-03-20 22:24:39 +0200

    mufat

---
+++ Assets/Scripts/CollectCoins.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectCoins : MonoBehaviour
+{
+    [Tooltip("The particles that appear after the player collects a coin.")]
+    public GameObject coinParticles;
+
+    [Tooltip("Amount of health to add to the player when collected.")]
+    public float healthToAdd = 5f;
+
+    PlayerMovement playerMovementScript;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playerMovementScript = other.GetComponent<PlayerMovement>();
+            //playerMovementScript.ModifyHealth(healthToAdd);
+            playerMovementScript.soundManager.PlayCoinSound();
+            playerMovementScript.ModifyHealth(5, "coin collected");
+            ScoreManager.score += 10;
+            GameObject particles = Instantiate(coinParticles, transform.position, new Quaternion());
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PatrolBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolBehavior : MonoBehaviour
+{
+    [Tooltip("The transform to which the enemy will pace back and forth to.")]
+    public Transform[] patrolPoints;
+
+    private int currentPatrolPoint = 0;
+
+    public void Patrol(float walkSpeed)
+    {
+        Vector3 moveToPoint = patrolPoints[currentPatrolPoint].position;
+        transform.position = Vector3.MoveTowards(transform.position, moveToPoint, walkSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, moveToPoint) < 0.01f)
+        {
+            currentPatrolPoint++;
+            if (currentPatrolPoint > patrolPoints.Length - 1)
+            {
+                currentPatrolPoint = 0;
+            }
+        }
+    }
+}
